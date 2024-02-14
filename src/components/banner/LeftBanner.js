a0e0bc3d58d04aec719b3fc91f89474f1c882b8c
Imesh Premjayanth <supunimesh99@gmail.com>
2024-02-14 13:04:39 +0530

    first commit

---
+++ src/components/banner/LeftBanner.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+import Media from './Media';
+
+const LeftBanner = () => {
+    const [text] = useTypewriter({
+      words: ["Undergraduate Student"],
+      loop: true,
+      typeSpeed: 20,
+      deleteSpeed: 10,
+      delaySpeed: 2000,
+    });
+  return (
+    <div className="w-full lgl:w-1/2 flex flex-col gap-20">
+      <div className="flex flex-col gap-5">
+        <h4 className=" text-lg font-normal">WELCOME TO MY WORLD</h4>
+        <h1 className="text-6xl font-bold text-white">
+          Hi, I'm <span className="text-designColor capitalize">Imesh Premjayanth</span>
+        </h1>
+        <h2 className="text-4xl font-bold text-white">
+          a <span>{text}</span>
+          <Cursor
+            cursorBlinking="false"
+            cursorStyle="|"
+            cursorColor="#ff014f"
+          />
+        </h2>
+        <p className="text-base font-bodyFont leading-6 tracking-wide">
+          I am a highly motivated person with a keen interested absorbing new experience. I am looking for an opportunity
+         to prove my worth and advance my career as far as possible. I consider myself to be an effecctive team player with excellent 
+         leadership skills as well.
+        </p>
+      </div>
+     {/* Media */}
+     <Media />
+    </div>
+  );
+}
+
+export default LeftBanner
